b3042bfa89fae5f0b9e1aaa90ef61a27f3871ac2
Bopchy <rorbochere@gmail.com>
2022-03-03 23:58:58 +0300

    Add paragraph Typography.

--- audiophile-ecommerce-website/client/src/components/Typography.js
+++ audiophile-ecommerce-website/client/src/components/Typography.js
@@ -1,5 +1,14 @@
 import { styled } from "@linaria/react";
 
+const MANROPE = `font-family: "Manrope", sans-serif;`;
+
+const Header = `
+  ${MANROPE}
+  font-weight: ;
+  font-size: ;
+  font-style: ;
+`;
+
 export const Paragraph = styled.p`
   margin: 0;
   font-size: ;
